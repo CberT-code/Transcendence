57fc75e626a30ce97d91c44cf3697a003e366c99
Bertola Cyrille <cyrille.bertola@garantid.com>
2021-02-03 17:06:14 +0100

    not working

--- WebSite/Pong/app/controllers/pages_controller.rb
+++ WebSite/Pong/app/controllers/pages_controller.rb
@@ -1,4 +1,8 @@
 class PagesController < ApplicationController
+	if $token.expired?
+		$client = OAuth2::Client.new(UID, SECRET, site: "https://api.intra.42.fr")
+		$token = $client.client_credentials.get_token
+	end
 	def salut
 		@name = params[:name]
 	end
--- WebSite/Pong/config/routes.rb
+++ WebSite/Pong/config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
 	root to: 'pages#home'
 	get '/bonjour(/:name)', to: 'pages#salut', as: 'salut'
-	get '/authentification', to: 'authentification#responseAuth', as: 'responseAuth'
+	get '/authentification', to: 'users/omniauth_callbacks#marvin', as: 'marvin'
 	get '/connexion', to: 'authentification#connexion', as: 'connexion'
+	devise_for :users, controllers: { omniauth_callbacks: "users/omniauth_callbacks" }
+	  devise_scope :user do
+		delete "/users/sign_out", to: "devise/sessions#destroy", as: 'sign_out'
+	  end
 end
