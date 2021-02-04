0989e156061ba8745590da095f214fff9f8dbe6e
Bertola Cyrille <cyrille.bertola@garantid.com>
2021-02-04 10:09:20 +0100

    working - working on oauth

--- WebSite/Pong/config/routes.rb
+++ WebSite/Pong/config/routes.rb
@@ -3,8 +3,5 @@ Rails.application.routes.draw do
 	get '/bonjour(/:name)', to: 'pages#salut', as: 'salut'
 	get '/authentification', to: 'users/omniauth_callbacks#marvin', as: 'marvin'
 	get '/connexion', to: 'authentification#connexion', as: 'connexion'
-	devise_for :users, controllers: { omniauth_callbacks: "users/omniauth_callbacks" }
-	  devise_scope :user do
-		delete "/users/sign_out", to: "devise/sessions#destroy", as: 'sign_out'
-	  end
+
 end
