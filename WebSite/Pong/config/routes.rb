e85e85d199ba8e129d7e41123b73577e87d23701
hbaudet <hbaudet@student.42.fr>
2021-02-04 19:20:02 +0100

    scripted backup

--- WebSite/Pong/config/routes.rb
+++ WebSite/Pong/config/routes.rb
@@ -1,10 +1,8 @@
 Rails.application.routes.draw do
+	devise_for :users, controllers: { omniauth_callbacks: "users/omniauth_callbacks" }
 	root to: 'pages#home'
 	get '/bonjour(/:name)', to: 'pages#salut', as: 'salut'
-	get '/authentification', to: 'users/omniauth_callbacks#marvin', as: 'marvin'
-	get '/connexion', to: 'authentification#connexion', as: 'connexion'
-	devise_for :users, controllers: { omniauth_callbacks: "users/omniauth_callbacks" }
-	  devise_scope :user do
-		delete "/users/sign_out", to: "devise/sessions#destroy", as: 'sign_out'
+	devise_scope :user do
+		delete 'sign_out', :to => 'devise/sessions#destroy', :as => :destroy_user_session_path
 	  end
 end
