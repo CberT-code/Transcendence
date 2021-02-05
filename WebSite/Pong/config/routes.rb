9ee3635bd73c3db98e9a6f8bb01accf82c4dea73
Nakusu <leopold.lepage@gmail.com>
2021-02-05 15:59:37 +0100

    add of javascript, backbone and other

--- WebSite/Pong/config/routes.rb
+++ WebSite/Pong/config/routes.rb
@@ -1,7 +1,3 @@
 Rails.application.routes.draw do
-	root to: 'pages#home'
-	get '/bonjour(/:name)', to: 'pages#salut', as: 'salut'
-	get '/authentification', to: 'users/omniauth_callbacks#marvin', as: 'marvin'
-	get '/connexion', to: 'authentification#connexion', as: 'connexion'
-
+	root to: 'application#index'
 end
