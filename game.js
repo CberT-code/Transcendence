40374771571079c308bf8c1dc0b6291f40f372ba
hbaudet <hbaudet@student.42.fr>
2021-02-07 01:08:50 +0100

    removed useless comments

--- game.js
+++ game.js
@@ -1,33 +1,3 @@
-/*
-var character = document.getElementById("character");
-
-document.addEventListener("click",jump);
-
-function jump(){
-    if(character.classList == "animate"){
-        return;
-    }
-    character.classList.add("animate");
-    setTimeout(removeJump,300); //300ms = length of animation
-};
-
-function removeJump(){
-    character.classList.remove("animate");
-}
-
-var block = document.getElementById("block");
-
-function checkDead(){
-    let characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
-    let blockLeft = parseInt(window.getComputedStyle(block).getPropertyValue("left"));
-    if(blockLeft<20 && blockLeft>-20 && characterTop>=130){
-        alert("Game over");
-        //block.style.left = '480px';
-    }
-}
-
-setInterval(checkDead, 10);
-*/
 var user = document.getElementById("user");
 var oppo = document.getElementById("opponent");
 var ball = document.getElementById("ball");
