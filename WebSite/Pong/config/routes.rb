1e8427088d437f963b14c74cfa4b11ff9b88b724
hbaudet <hbaudet@student.42.fr>
2021-02-08 09:30:09 +0100

    forking

--- WebSite/Pong/config/routes.rb
+++ WebSite/Pong/config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
-	root to: 'pages#home'
-	get '/bonjour(/:name)', to: 'pages#salut', as: 'salut'
-	get '/authentification', to: 'users/omniauth_callbacks#marvin', as: 'marvin'
-	get '/connexion', to: 'authentification#connexion', as: 'connexion'
 	devise_for :users, controllers: { omniauth_callbacks: "users/omniauth_callbacks" }
-	  devise_scope :user do
-		delete "/users/sign_out", to: "devise/sessions#destroy", as: 'sign_out'
+	root to: 'pages#home'
+	get '/test', to: 'pages#test', as 'truc'
+	get '/bonjour(/:name)', to: 'pages#salut', as: 'salut'
+	devise_scope :user do
+		delete 'sign_out', :to => 'devise/sessions#destroy', :as => :destroy_user_session_path
 	  end
 end
